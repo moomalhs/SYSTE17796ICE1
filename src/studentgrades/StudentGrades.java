9b060ec535a7c80d03e0b346653918e7064e50ef
moomalharoon <moomalharoon@192.168.2.25>
2021-02-09 21:16:06 -0500

    first commit

---
+++ src/studentgrades/StudentGrades.java
@@ -0,0 +1,31 @@
+
+package studentgrades;
+
+
+public class StudentGrades {
+     
+   
+    public static void main(String[] args) {
+        
+     double[] grade = {10,23,21,24,67,45,89,67,4.5,8.9};
+     
+      System.out.println(calcAvg(grade));
+    
+      
+      
+
+    }
+
+    private static double calcAvg(double[] grade) {
+      double result = 0;
+      
+      for (double g : grade) {
+          result += g;
+      }
+    
+
+      return result / grade.length;
+      
+}  
+}
+
